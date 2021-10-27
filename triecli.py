cb9d97eb2d9bf1a3dc17050373f44a551290630e
Nathaniel Thomas <55164518+Nathaniel-github@users.noreply.github.com>
2021-10-27 14:59:23 -0700

    Add direct functionality

--- triecli.py
+++ triecli.py
@@ -10,30 +10,61 @@ def cli():
 
 
 @cli.command()
-@click.option('-a', '--add', type=str, help='Value to add to the trie', nargs=1)
-def direct():
-
+@click.argument(type=str, help='Adds the given word to the trie', nargs=1)
+def add(val:str):
+    request('Add keyword', val)
 
 
 @cli.command()
+@click.argument(type=str, help='Deletes the given word from the trie', nargs=1)
+def delete(val:str):
+    request('Delete keyword', val)
+
+
+@cli.command()
+@click.argument(type=str, help='Searches the trie to see if the given word exists', nargs=1)
+def search(val:str):
+    request('Search for keyword', val)
+
+
+@cli.command()
+@click.argument(type=str, help='Autocompletes the prefix from the known values in the trie', nargs=1)
+def complete(val:str):
+    request('Autocomplete by prefix', val)
+
+
+@cli.command(help='Displays the trie using recursion (slower)')
+def view():
+    request('Display trie')
+
+
+@cli.command(help='Displays the trie from the pre-stored array values (faster)')
+def viewfast():
+    request('Display trie fast')
+
+
+@cli.command(help='Runs the CLI with a nice user interface')
 def ui():
     question = [
         inquirer.List('choice',
                       message='What operation would you like to perform',
                       choices=['Add keyword', 'Delete keyword', 'Search for keyword', 'Autocomplete by prefix',
-                               'Display trie', 'Display trie fast'],
+                               'Display trie', 'Display trie fast', 'Exit'],
                       carousel=True)
     ]
+    question2 = [
+        inquirer.Text('val',
+                      message='Enter a word')
+    ]
+    while True:
+        choice = inquirer.prompt(question, theme=GreenPassion())['choice']
 
-    choice = inquirer.prompt(question, theme=GreenPassion())['choice']
+        if choice == 'Exit':
+            quit()
 
-    if choice != 'Display trie' and choice != 'Display trie fast':
-        question2 = [
-            inquirer.Text('val',
-                          message='Enter a word')
-        ]
-        val = inquirer.prompt(question2, theme=GreenPassion())['val']
-        request(choice, val)
-    else:
-        request(choice)
+        if choice != 'Display trie' and choice != 'Display trie fast':
+            val = inquirer.prompt(question2, theme=GreenPassion())['val']
+            request(choice, val)
+        else:
+            request(choice)
 
